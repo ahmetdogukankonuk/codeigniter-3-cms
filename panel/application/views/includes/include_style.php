e5b56f79c545396bc1a60ba4db38643fd26fc2c7
ahmetdogukankonuk <ahmetdogukankonuk@gmail.com>
2022-09-27 14:15:18 +0300

    Alert system created

--- panel/application/views/includes/include_style.php
+++ panel/application/views/includes/include_style.php
@@ -4,4 +4,5 @@
         <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.bootstrap5.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
         <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo base_url("assets"); ?>/css/iziToast.min.css">
         <link rel="stylesheet" href="<?php echo base_url("assets"); ?>/css/style.css">
